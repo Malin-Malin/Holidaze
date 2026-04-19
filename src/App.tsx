db76a132135650b482ba0b01e664f81b044e9ceb
Malin Skrettingland <malinersdal@hotmail.com>
2026-04-19 22:42:00 +0200

    feat: set up routing for venue details and grid in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
 import "./App.css";
-import VenuesPage from "./pages/venueGrid";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import VenuesGrid from "./pages/venueGrid";
+import VenueDetail from "./pages/venueDetail";
 
 function App() {
-  return <VenuesPage />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<VenuesGrid />} />
+        <Route path="/venue/:id" element={<VenueDetail />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
