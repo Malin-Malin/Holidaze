82a83cbd2f4bb61130d6e350fc329f944e187cf2
Malin Skrettingland <malinersdal@hotmail.com>
2026-04-20 22:47:27 +0200

    feat: add footer component

---
+++ src/components/footer.tsx
@@ -0,0 +1,12 @@
+<footer>
+  footerlogo
+  <nav>
+    <a href="/">Home</a>
+    <a href="/venues">Venues</a>
+    <a href="/profile">Profile</a>
+  </nav>
+  <span>Popular travels</span>
+  list of popular travels
+  <div>footersearch</div>
+  info about the company contact info social media links copyright notice
+</footer>;
