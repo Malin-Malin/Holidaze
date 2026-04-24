8df82c349b40566a9ba1823901f68aebba52ee24
Malin Skrettingland <malinersdal@hotmail.com>
2026-04-24 20:36:40 +0200

    feat: implement conditional banner on veenueDetails

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,32 @@
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import { Header } from "./components/layout/header";
 import { Banner } from "./components/layout/banner";
 import VenuesGrid from "./pages/venueGrid";
 import VenueDetail from "./pages/venueDetail";
 import { Footer } from "./components/layout/footer";
 
-function App() {
+function AppLayout() {
+  const location = useLocation();
+  const showBanner = !location.pathname.startsWith("/venue/");
+
   return (
-    <BrowserRouter>
+    <>
       <Header />
-      <Banner />
+      {showBanner && <Banner />}
       <Routes>
         <Route path="/" element={<VenuesGrid />} />
         <Route path="/venue/:id" element={<VenueDetail />} />
       </Routes>
       <Footer />
+    </>
+  );
+}
+
+function App() {
+  return (
+    <BrowserRouter>
+      <AppLayout />
     </BrowserRouter>
   );
 }
