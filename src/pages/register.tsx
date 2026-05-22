796099283363e472638c4b7edc8c0be313a4c0fe
Malin Skrettingland <malinersdal@hotmail.com>
2026-05-22 17:58:45 +0200

    feat: implement protected authentication routes and redirects for login and registration pages

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,6 +1,20 @@
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+
 import { RegisterForm } from "../components/auth/RegisterForm";
 
+import { useAuth } from "../hooks/useAuth";
+
 const RegisterPage = () => {
+  const { isLoggedIn } = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (isLoggedIn) {
+      navigate("/profile", { replace: true });
+    }
+  }, [isLoggedIn, navigate]);
+
   return (
     <section
       className="banner-hero bg-cover bg-center w-screen ml-[calc(50%-50vw)] p-6 min-h-[max(130px,calc(100dvh-20rem))]"
